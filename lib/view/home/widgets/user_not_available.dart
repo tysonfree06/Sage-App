21af1cd56d59c920ac7a47345b1f9162f94dac42
Muhammad Saad Shafiq <mlbenchpvtltd@gmail.com>
2025-07-01 12:45:56 +0500

    Add Countdown Timer and Refactor Home Screen Layout  - Introduced CountdownTimerWidget to display the time remaining until the next anniversary. - Updated HomeScreen to include the CountdownTimerWidget with a target date. - Refactored user content layout for better organization and readability. - Enhanced UserNotAvailableWidget with improved styling and spacing. - Added new points icon asset for better visual representation.

--- lib/view/home/widgets/user_not_available.dart
+++ lib/view/home/widgets/user_not_available.dart
@@ -19,24 +19,54 @@ class UserNotAvailableWidget extends StatelessWidget {
       ),
       child: Column(
         children: [
+          SizedBox(height: 40.h),
           Assets.icons.addFilled.svg(),
           SizedBox(height: 8.h),
-          Text('Add Partner', style: context.typography.body),
-          Text('or', style: context.typography.body),
-          Text('Share Code', style: context.typography.body),
+          Text(
+            'Add Partner',
+            style: context.typography.body.copyWith(
+              color: context.colors.white,
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 72.h),
+          Text(
+            'Or',
+            style: context.typography.body.copyWith(
+              color: context.colors.white.withValues(alpha: .50),
+              fontSize: 11.sp,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(
+            'Share Code',
+            style: context.typography.body.copyWith(
+              color: context.colors.white,
+              fontSize: 13.sp,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
           SizedBox(height: 10.h),
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 6.h),
+            padding: EdgeInsets.symmetric(horizontal: 9.w, vertical: 3.h),
             decoration: BoxDecoration(
-              color: context.colors.white.withOpacity(.1),
+              color: context.colors.white.withValues(alpha: .1),
               borderRadius: BorderRadius.circular(AppRadiuses.hundredRadius),
             ),
             child: Row(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('546789', style: context.typography.body),
+                Text(
+                  '546789',
+                  style: context.typography.body.copyWith(
+                    color: context.colors.white.withValues(alpha: .6),
+                    fontSize: 11.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
                 SizedBox(width: 8.w),
-                Assets.icons.copy.svg(),
+                Assets.icons.copy.svg(height: 12.h, width: 12.w),
               ],
             ),
           ),
